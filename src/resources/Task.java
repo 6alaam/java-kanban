176ead81cd24163d5a5e98b7dae7aa115de04340
6alaam <ballaam@yandex.ru>
2024-11-05 14:27:59 +0300

    Add files via upload

--- src/resources/Task.java
+++ src/resources/Task.java
@@ -8,9 +8,9 @@ public class Task {
     private String description;
     private Status status;
 
-    public Task(int id, String title, String description, Status status) {
+    public Task(int id, String name, String description, Status status) {
         this.id = id;
-        this.name = title;
+        this.name = name;
         this.description = description;
         this.status = status;
     }
@@ -18,22 +18,31 @@ public class Task {
     public int getId() {
         return id;
     }
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     public Status getStatus() {
         return status;
     }
-
     public void setStatus(Status status) {
         this.status = status;
     }
+
 
     @Override
     public boolean equals(Object o) {
@@ -45,12 +54,12 @@ public class Task {
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return id;
     }
-
     // переопределил для тестов
+
     @Override
-    public String toString() {
+        public String toString() {
         return "Task{" +
                 "name='" + name + '\'' +
                 ", description='" + description + '\'' +
