45a37831b88288dc50d3011c338852f2f3300aed
Balaam <ballaam@yandex.ru>
2025-01-14 13:34:38 +0300

    гитхаб перестал видеть тесты

---
+++ src/Test/manage/ManagerTest.java
@@ -0,0 +1,19 @@
+package Test.manage;
+
+import manage.Manager;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+class ManagerTest {
+
+    @Test
+    void getDefaultShouldInitializeInMemoryTaskManager() {
+        assertInstanceOf(InMemoryTaskManager.class, Manager.getDefault());
+    }
+
+    @Test
+    void getDefaultHistoryShouldInitializeInMemoryHistoryManager() {
+        assertInstanceOf(InMemoryHistoryManager.class, Manager.getDefaultHistory());
+    }
+}
---
+++ src/Test/resources/SubtaskTest.java
@@ -0,0 +1,18 @@
+package Test.resources;
+
+import org.junit.jupiter.api.Test;
+import resources.Status;
+import resources.Subtask;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SubtaskTest {
+
+    @Test
+    public void SubtasksWithEqualIdShouldBeEqual() {
+        Subtask subtask1 = new Subtask(10, "Купить хлеб", "В Дикси у дома", Status.NEW, 5);
+        Subtask subtask2 = new Subtask(10, "Купить молоко", "В Пятерочке", Status.DONE, 5);
+        assertEquals(subtask1, subtask2,
+                "Ошибка! Наследники класса Task должны быть равны друг другу, если равен их id;");
+    }
+}
