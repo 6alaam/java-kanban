793c4099375ebae63f448d20f5237bc2386490e8
Balaam <ballaam@yandex.ru>
2025-01-14 15:02:47 +0300

    попытка 5

--- src/test/resources/SubtaskTest.java
+++ src/test/resources/SubtaskTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SubtaskTest {
 
     @Test
-    public void SubtasksWithEqualIdShouldBeEqual() {
+    public void subtasksWithEqualIdShouldBeEqual() {
         Subtask subtask1 = new Subtask(10, "Купить хлеб", "В Дикси у дома", Status.NEW, 5);
         Subtask subtask2 = new Subtask(10, "Купить молоко", "В Пятерочке", Status.DONE, 5);
         assertEquals(subtask1, subtask2,
