8319cf7b578ff2777b13e6f90c2572f10acc4619
Balaam <ballaam@yandex.ru>
2025-01-15 13:39:29 +0300

    попытка 25

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -1,11 +1,11 @@
 package service;
 
 public interface Managers {
-//    public static InMemoryTaskManager getDefault() {
-//        return new InMemoryTaskManager();
-//    }
+    public static InMemoryTaskManager getDefault() {
+        return new InMemoryTaskManager();
+    }
 
-    // по первому методу ообще вопрос, это осколок от прошлого тз ,сейчас оно есть только в тестах, смысла в этом методе не
+    // по первому методу ообщее вопрос, это осколок от прошлого тз ,сейчас оно есть только в тестах, смысла в этом методе не
 
 
 
