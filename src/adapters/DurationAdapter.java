ab4e2f7120ce6822c0dc96df53c9e414603a9393
Balaam <ballaam@yandex.ru>
2025-05-07 15:49:33 +0300

    тесты просто огонь1

--- src/adapters/DurationAdapter.java
+++ src/adapters/DurationAdapter.java
@@ -2,6 +2,7 @@ package adapters;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
@@ -9,13 +10,20 @@ import java.time.Duration;
 
 public class DurationAdapter extends TypeAdapter<Duration> {
     @Override
-    public void write(JsonWriter out, Duration duration) throws IOException {
-        out.value(duration.getSeconds());
+    public void write(JsonWriter jsonWriter, Duration duration) throws IOException {
+        if (duration == null || duration == Duration.ZERO) {
+            jsonWriter.nullValue();
+        } else {
+            jsonWriter.value(duration.toMinutes());
+        }
     }
 
     @Override
-    public Duration read(JsonReader in) throws IOException {
-        long seconds = in.nextLong();
-        return Duration.ofSeconds(seconds);
+    public Duration read(JsonReader jsonReader) throws IOException {
+        if (jsonReader.peek() == JsonToken.NULL) {
+            jsonReader.nextNull();
+            return Duration.ZERO;
+        }
+        return Duration.ofMinutes(jsonReader.nextLong());
     }
 }
